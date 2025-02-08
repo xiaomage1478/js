53a8198fd22a8d57ed965ac15513c85c577da0be
xiaomage1478 <354938940@qq.com>
2025-02-08 15:17:45 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,11 @@
 <script type="text/javascript">
-    alert("你好");
+    window.onload = function() {
+    function infiniteAlert() {
+        alert("你好！");
+        // 递归调用，以确保alert框一直弹出
+        infiniteAlert();
+    }
+    
+    infiniteAlert();
+}
 </script>
